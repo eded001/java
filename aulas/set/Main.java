a9317b49b7683599fda270b3af23eaf0d7a8f693
ed <edgarams09@gmail.com>
2024-07-03 12:02:12 -0300

    estudo de Sets

---
+++ aulas/set/Main.java
@@ -0,0 +1,17 @@
+// package aulas.set;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String args[]) {
+        Set<String> nomes = new HashSet<>();
+        nomes.add("edgar");
+        nomes.add("natacha");
+        nomes.add("edgar"); // não vai ser adicionado
+
+        for(String str : nomes) {
+            System.out.println(str);
+        }
+    }
+}
